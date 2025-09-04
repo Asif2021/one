4b066e498b6d4ffc2c25b2bc0b48c6b9d2117ee4
ramzan617 <asiframzan2021@gmail.com>
2025-09-04 11:58:04 +0500

    update CaseStudy component

--- components/ui/caseStudy.jsx
+++ components/ui/caseStudy.jsx
@@ -8,11 +8,13 @@ import { useEffect, useState } from "react";
 
 export default function CaseStudy() {
     const [index, setIndex] = useState(0);
+    const [heading, setHeading] = useState(0);
 
      const images = ["/images/3.jpg", "/images/4.jpg", "/images/5.jpg"];
+     const headings = ["Sazgar", "HBK Retail Park", "Seamless"];
      const data = [
         {
-            heading: "Seamless",
+            title: "Sazgar",
             description: "Outsourced their operational core with a contact center in place",
             stats:[
                 {value:"+88", label:"conversion rate"},
@@ -20,7 +22,7 @@ export default function CaseStudy() {
             ]
         },
         {
-            heading: "HBK Retail Park",
+            title: "HBK Retail Park",
             description: "Revolutionized shopping experience through SAP Business One.",
             stats:[
                 {value:"+98", label:"conversion rate"},
@@ -28,7 +30,7 @@ export default function CaseStudy() {
             ]
         },
         {
-            heading: "Seamless",
+            title: "Seamless",
             description: "Outsourced their operational core with a contact center in place",
             stats:[
                 {value:"+100", label:"conversion rate"},
@@ -39,48 +41,64 @@ export default function CaseStudy() {
       useEffect(() => {
     const interval = setInterval(() => {
      setIndex((prev) => (prev + 1) % images.length);
-    }, 1500); 
+     setHeading((prev) => (prev + 1) % headings.length);
+    }, 2000); 
 
     return () => clearInterval(interval);
   }, [images.length]);
 
 
   return (
-<div className="max-w-6xl mx-auto px-8 pt overflow-hidden my-10 md:mt-0 bg-black text-white rounded-4xl">
+<div className="max-w-6xl mx-auto px-8 pt-8 overflow-hidden my-10 md:mt-0 bg-black text-white rounded-4xl">
         <h1 className="text-white text-5xl md:text-xl font-bold">Case Studies</h1>
     <div className=" hidden md:grid grid-cols-2 gap-4 mt-2 mb-5">
         <h1 className="font-bold text-3xl">Innovation at Every Step</h1>
         <h3 className="pr-20">Embrace the power of cutting-edge technology with us to accelerate growth and drive your business forward.</h3>
     </div>
     <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-12 gap-6">
-  <div className="col-span-1 sm:col-span-1 md:col-span-3 flex flex-row md:flex-col justify-start p-4">
-    <h1>Sazgar</h1>
-    <h1 className="my-3">HBK Retail Park</h1>
-    <h1>Seamless</h1>
+  <div className="col-span-1 sm:col-span-1 md:col-span-3 flex flex-row md:flex-col gap-5 justify-start p-4 text-gray-500">
+  {headings.map((title, i)=>(
+  <motion.h1
+  key={title}
+  className="flex items-start"
+  animate={{ color: heading === i ? "#f97316" : "#808080" }}
+  transition={{ duration: 0.8, ease: "easeInOut" }}
+>
+  <motion.span
+    animate={{ borderColor: heading === i ? "#f97316" : "#808080" }}
+    transition={{ duration: 0.8, ease: "easeInOut" }}
+    className="border-t-2 md:border-t-0 md:border-l-2 h-0 md:h-8 w-full md:w-8 mr-0 mb-4 md:mb-0"
+  />
+  {title}
+</motion.h1>
+  ))}
   </div>
 
-  <div className="col-span-1 sm:col-span-1 md:col-span-4 ">
+  <div className="col-span-1 sm:col-span-1 md:col-span-4 mb-8 ">
     <AnimatePresence mode="wait">
         <motion.div
             key={index}
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 50 }}
-            transition={{ duration: 0.8, ease: "easeInOut" }}
-          >
-            <h1 className="text-2xl font-bold">{data[index].heading}</h1>
+            transition={{ duration: 0.8, ease: "easeInOut" }}>
+        <h1 className="text-2xl font-bold">{data[index].title}</h1>
         <p className="mt-2">{data[index].description}</p>
 
         <div className="flex flex-row justify-between mt-4">
           {data[index].stats.map((stat, i) => (
             <div key={i}>
-              <h1 className="text-6xl text-orange-500">{stat.value}</h1>
+               <h1
+        className={`text-6xl ${
+          index === 1 ? "text-cyan-500" : "text-orange-500"
+        }`}
+      >{stat.value}</h1>
               <p>{stat.label}</p>
             </div>
           ))}
         </div>
 
-        <button className="py-2 px-3 font-bold mt-8 text-black bg-white rounded-xl">
+        <button className="py-2 px-3 font-bold mt-8 text-black hover:text-white bg-white hover:bg-black rounded-xl">
           view case study
         </button>
           </motion.div>
@@ -100,10 +118,10 @@ export default function CaseStudy() {
       src={images[index]}
       alt={`image-${index}`}
       width={440}
-      height={250}
+      height={350}
       priority
       loading="eager"
-      className="w-full h-64 object-cover rounded-tl-4xl rounded-br-4xl"
+      className="w-full h-72 object-cover rounded-tl-4xl rounded-br-4xl"
     />
   </motion.div>
 </AnimatePresence>
