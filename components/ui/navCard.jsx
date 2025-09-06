44e44d07ef2df650586730e2b148c8732425c84b
ramzan617 <asiframzan2021@gmail.com>
2025-09-06 19:14:01 +0500

    feat: remove Card component and integrate NavCards into Navbar for enhanced navigation

---
+++ components/ui/navCard.jsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import { MoveRight, ArrowRight  } from 'lucide-react';
+
+
+const NavCards = () => {
+  return (
+    <div className="grid grid-cols-3 gap-8">
+      <div className="flex flex-col gap-6">
+        <h1 className="font-bold">Munshi</h1>
+          <Image
+             src="/images/4.jpg"
+              alt="image"
+              width={240}
+              height={150}
+              className="w-full h-auto object-cover rounded-xl"
+            />
+   <Link href="#" className="hover:underline flex items-start p-5">
+  <span>Payment management solution for cash disbursements and approvals</span>
+  <ArrowRight className="" />
+</Link>   
+      </div>
+           <div className="flex flex-col gap-6">
+        <h1 className="font-bold">Cams</h1>
+          <Image
+             src="/images/4.jpg"
+              alt="image"
+              width={240}
+              height={150}
+              className="w-full h-auto object-cover rounded-xl"/>
+   <Link href="#" className="hover:underline flex items-start p-5">
+  <span>Payment management solution for cash disbursements and approvals</span>
+  <ArrowRight className="" />
+</Link>       </div>
+            <div className="flex flex-col gap-6">
+        <h1 className="font-bold">E-Tickets</h1>
+          <Image
+             src="/images/4.jpg"
+              alt="image"
+              width={240}
+              height={150}
+              className="w-full h-auto object-cover rounded-xl"
+            />
+            <Link href="#" className="hover:underline flex items-start p-5">
+  <span>Payment management solution for cash disbursements and approvals</span>
+  <ArrowRight className="" />
+</Link>  
+      </div>
+
+    </div>
+    
+  )
+}
+
+export default NavCards;
--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -1,11 +1,14 @@
 "use client";
 import { useState, useEffect } from "react";
 import { Menu, X, ChevronDown } from "lucide-react";
+import NavCards from "../ui/navCard"
 
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+  
+  
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,23 +18,27 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+
   const links = [
   {
     name: 'Services',
     href: '#',
     icon: ChevronDown,
+    card: <NavCards/>,
     description: 'Explore our range of services services, including web development, mobile app development, and more, and more and more and more and more and more and more and more',
   },
   {
     name: 'Products',
     href: '#',
     icon: ChevronDown,
+    card: <NavCards/>,
     description: 'Explore our range of services services, including web development, mobile app development, and more, and more and more and more and more and more and more and more',
   },
   {
     name: 'Projects',
     href: '#',
     icon: ChevronDown,
+   card: <NavCards/>,
     description: 'Explore our range of services services, including web development, mobile app development, and more, and more and more and more and more and more and more and more',
   },
   { name: 'Blogs', href: '#', icon: null },
@@ -40,9 +47,9 @@ export default function Navbar() {
   return (
     <nav
       className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
-        scrolled || isOpen
-          ? "backdrop-blur-md bg-white/60 shadow-md text-black"
-      : "bg-transparent text-white"
+        scrolled
+          ? "backdrop-blur-md bg-white/60 shadow-md text-black hidden md:block"
+      : "bg-transparent text-orange-500 md:text-white"
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -57,7 +64,7 @@ export default function Navbar() {
               onClick={() => setIsOpen(!isOpen)}
               className="p-2 text-gray-800 focus:outline-none"
             >
-              {isOpen ? <X size={28} className="text-current" /> : <Menu size={28} className="inherit" />}
+              {isOpen ? <X size={28} className="text-orange-500" /> : <Menu size={28} className="text-orange-500" />}
             </button>
           </div>
 
@@ -82,11 +89,11 @@ export default function Navbar() {
                      invisible opacity-0 group-hover:visible group-hover:opacity-100 
                      transition-all duration-200"
         >
-          <div className="max-w-6xl mx-auto px-8 pt-8 overflow-hidden my-10 md:mt-0 
+          <div className="max-w-7xl mx-auto px-8 pt-8 overflow-hidden my-10 md:mt-0 
                           bg-white text-black rounded-4xl shadow-lg 
                           min-h-[250px] w-[80vw]">
             <div className="text-gray-700">
-              {item.name} {item.description}
+              {item.card}
             </div>
           </div>
         </div>
@@ -107,7 +114,7 @@ export default function Navbar() {
       {/* Mobile Dropdown */}
       {isOpen && (
         <div className="md:hidden bg-white h-screen shadow-lg">
-          <div className="w-full max-w-2xl mx-auto divide-y divide-gray-200 rounded-xl border border-gray-200">
+          <div className="w-full max-w-2xl mx-auto divide-gray-200 rounded-xl">
   {links.map((item, i) => (
     <div key={i} className="group">
       {/* Accordion Header */}
